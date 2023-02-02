7df61a1afb5c3e84131c9805a3a5d6f3ebdef406
baubau09 <katietran.182@gmail.com>
2023-02-02 15:14:10 +0700

    update find nums with even number of digits

--- src/1295_Find Numbers with Even Number of Digits.java
+++ src/1295_Find Numbers with Even Number of Digits.java
@@ -38,4 +38,17 @@ class FindNumbersWithEvenNumberofDigits {
         }
         return countEven;
     }
+
+
+    // Digit count of N = upper bound of log10(N).
+    public static int findNumbers2(int[] nums) {
+        int count = 0;
+        for (int num : nums) {
+            int digits = (int) Math.floor(Math.log10(num) + 1);
+            if (digits % 2 == 0) {
+                count++;
+            }
+        }
+        return count;
+    }
 }
